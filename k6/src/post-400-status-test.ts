e31db09ebb63c8ae39bb0fd3337af68c034adb17
sylhare <sylhare@outlook.com>
2024-03-21 09:39:39 -0400

    Update 400 example

--- k6/src/post-400-status-test.ts
+++ k6/src/post-400-status-test.ts
@@ -6,12 +6,17 @@ import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 import http from 'k6/http';
 
 export let options:Options = {
-  vus: 50,
+  vus: 5,
   duration: '10s'
 };
 
+function logResponse(response: { body: any }) {
+  console.log(response.body);
+}
+
 export default () => {
   const res = http.post('https://httpbin.org/status/400');
+  logResponse(res);
   check(res, {
     'status is 400': () => res.status === 400,
   });
