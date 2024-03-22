8196da776a268bd2e2d3344002b1198f5fcb6797
sylhare <sylhare@outlook.com>
2024-03-22 11:46:08 -0400

    Add custom trend

--- k6/src/get-200-status-test.ts
+++ k6/src/get-200-status-test.ts
@@ -1,16 +1,30 @@
-import { check, sleep } from 'k6';
+import { check, group, sleep } from 'k6';
 import { Options } from 'k6/options';
 import http from 'k6/http';
+import { Trend } from 'k6/metrics';
 
 export let options: Options = {
-  vus: 50,
-  duration: '10s',
+  vus: 5,
+  duration: '2s',
 };
 
+const customTrend = new Trend('trend_custom');
+const taggedTrend = new Trend('trend_tagged');
+
 export default function () {
-  const result = http.get('http://test.k6.io');
-  check(result, {
-    'status is 200': () => result.status === 200,
+
+  group('Browse test.k6.io', function () {
+    const result = http.get('http://test.k6.io', {
+      tags: { tag_key: 'tag_value' },
+    });
+
+    check(result, {
+      'status is 200': () => result.status === 200,
+      'body size is 11,105 bytes': () => !!JSON.stringify(result.body).match(/k6/),
+    });
+    sleep(1);
+
+    customTrend.add(result.timings.connecting);
+    taggedTrend.add(result.timings.connecting, { tag_connection_time: 'k6_website'});
   });
-  sleep(1);
 }
